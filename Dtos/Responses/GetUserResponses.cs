6fc43f067ee603cde11bf3c84d7ace3dd80b70a1
Deshanm123 <deshanm123@gmail.com>
2025-04-02 19:22:06 +0100

    initializing the repo

---
+++ Dtos/Responses/GetUserResponses.cs
@@ -0,0 +1,10 @@
+namespace AutoMapper.Dtos.Responses
+{
+    public class GetUserResponse
+    {
+        public string FullName{get;set;} = string.Empty;
+        public string Address{get;set;} = string.Empty;
+        public string Email{get;set;} = string.Empty;
+
+    }
+}
---
+++ Profiles/UserProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+
+namespace AutoMapper_DEmo.UserProfile
+{
+    //when auto mapper scans through the dll files,It gonna search for the AutoMapper's inbuilt Library
+    class UserProfile : Profile
+    {
+        //first object : source of information
+        //second object : destination of the information
+
+        
+    }
+}
