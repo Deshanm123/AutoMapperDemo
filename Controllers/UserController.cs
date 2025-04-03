38434ceba20c71fc73adc1141ed9fc4c43b2ccde
Deshanm123 <deshanm123@gmail.com>
2025-04-03 11:28:41 +0100

    Implementing GetUserResponse route

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,34 +1,38 @@
-// using AutoMapper.Dtos.Responses;
-// using AutoMapper;
-// using AutoMapper_DEmo.Models.DbSet;
+using AutoMapper;
+using AutoMapper_DEmo.Dtos.Responses;
+using AutoMapper_DEmo.Models.DbSet;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace AutoMapper_DEmo.Controllers;
+namespace AutoMapper_DEmo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
 
-// [ApiController]
-// [Route("[controller]")]
-//     public UserController : ControllerBase
-//     {
-//         // private readonly IMapper _mapper;
-//         // public UserController(IMapper mapper)
-//         // {
-//         //     _mapper = mapper;
-//         // }
+    public class UserController : ControllerBase
+    {
+            private readonly IMapper _mapper;
+            public UserController(IMapper mapper)
+            {
+                _mapper = mapper;
+            }
 
 
-//         // public IActionResult GetUserRespose()
-//         // {
-//         //     User moderateUser = new User {
-//         //         Id = new Guid(),
-//         //         FirstName = "John",
-//         //         LastName = "Doe",
-//         //         Email = "johndoe@mail.com",
-//         //         PostCode = "JH49DE",
-//         //         StreetAddress = "LoremIpsum Street",
-//         //     };
+            [HttpGet]
+            public IActionResult GetUserRespose()
+            {
+                User moderateUser = new User {
+                    Id = new Guid(),
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "johndoe@mail.com",
+                    PostCode = "JH49DE",
+                    StreetAddress = "LoremIpsum Street",
+                };
 
-//         //     //mapping
-//         //     GetUserResponse resp = _mapper.Map<GetUserResponse>(moderateUser);
-//         //     return Ok(resp);
-//         // }
-//     }
+                //mapping
+                GetUserResponse resp = _mapper.Map<GetUserResponse>(moderateUser);
+                return Ok(resp);
+            }
+    }
 
+}
