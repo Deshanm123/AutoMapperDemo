b9ec10ee70b02dee7b680ab5cc3e27194cd81a20
Deshanm123 <deshanm123@gmail.com>
2025-04-03 10:00:33 +0100

    updated files in gitignore

---
+++ Controllers/UserController.cs
@@ -0,0 +1,34 @@
+// using AutoMapper.Dtos.Responses;
+// using AutoMapper;
+// using AutoMapper_DEmo.Models.DbSet;
+
+//namespace AutoMapper_DEmo.Controllers;
+
+// [ApiController]
+// [Route("[controller]")]
+//     public UserController : ControllerBase
+//     {
+//         // private readonly IMapper _mapper;
+//         // public UserController(IMapper mapper)
+//         // {
+//         //     _mapper = mapper;
+//         // }
+
+
+//         // public IActionResult GetUserRespose()
+//         // {
+//         //     User moderateUser = new User {
+//         //         Id = new Guid(),
+//         //         FirstName = "John",
+//         //         LastName = "Doe",
+//         //         Email = "johndoe@mail.com",
+//         //         PostCode = "JH49DE",
+//         //         StreetAddress = "LoremIpsum Street",
+//         //     };
+
+//         //     //mapping
+//         //     GetUserResponse resp = _mapper.Map<GetUserResponse>(moderateUser);
+//         //     return Ok(resp);
+//         // }
+//     }
+
